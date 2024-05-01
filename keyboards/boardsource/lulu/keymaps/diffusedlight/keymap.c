f2556be6cdc74ba6b75b53050ee221a0f3b04add
diffusedlight <contact@reina.dev>
2024-05-01 06:59:49 +0100

    reverted back to symbol layer

--- keyboards/boardsource/lulu/keymaps/diffusedlight/keymap.c
+++ keyboards/boardsource/lulu/keymaps/diffusedlight/keymap.c
@@ -7,7 +7,7 @@
 enum layers {
     _ALPHA,
     _NUM,
-    _SYMBOL,
+    _SYM,
     _NAV,
     _VIM,
     _MEDIA,
@@ -39,7 +39,7 @@ enum layers {
 #define T_MEDIA LT(_MEDIA, KC_ESC)
 #define T_NAV   LT(_NAV, KC_SPC)
 #define T_VIM   LT(_VIM, KC_TAB)
-#define T_SYM   LT(_SYMBOL, KC_ENT)
+#define T_SYM   LT(_SYM, KC_ENT)
 #define T_NUM   LT(_NUM, KC_BSPC)
 #define T_FUN   LT(_FUN, QK_REP)
 
@@ -71,8 +71,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, KC_COMM,    KC_U,    KC_O,    KC_F,    KC_J,                     KC_V,    KC_C,    KC_D,    KC_L,    KC_B, XXXXXXX,
   XXXXXXX,  HOME_I,  HOME_E,  HOME_A,  HOME_H,    KC_Y,                     KC_G,  HOME_S,  HOME_T,  HOME_R,  HOME_N, XXXXXXX,
   XXXXXXX,  KC_DOT, KC_SCLN, KC_QUOT,    KC_P,    KC_K,KC_MINUS, KC_BSLS,   KC_Z,    KC_W,    KC_M,    KC_Q,    KC_X, XXXXXXX,
-                             OS_LSFT, T_MEDIA,   T_NAV,   T_VIM,   T_SYM,  T_NUM,   T_FUN, OS_LSFT
+                             OS_LSFT, T_MEDIA,   T_NAV,   T_VIM,  T_SYM,  T_NUM,   T_FUN, OS_LSFT
 ),
+
+/*  Symbol Layout - Miryoku Based 
+ * , U O F J - V C D L B
+ * I E A H Y - G S T R N 
+ * . ; ' P K - Z W M Q X
+ *
+ * Thumbs:
+ * OS_SFT ESC SPACE TAB - ENTER BKSPC REPEAT OS_SFT
+ * 
+ *
+ * ENCODER KEYS?
+ * _ - \
+ */
+
+// TODO: Update the layout to above
+ [_SYM] = LAYOUT(
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_LCBR,  KC_AMPR,KC_ASTR, KC_LPRN, KC_RCBR,                  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_TILD,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS,                  XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_HYPR,
+  XXXXXXX, KC_QUES, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE, XXXXXXX, XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                             XXXXXXX, KC_LPRN, KC_RPRN, KC_UNDS, _______,XXXXXXX, XXXXXXX, XXXXXXX
+),
+
 
 /*
  * Num Layout - HR Mods Right Side
@@ -84,33 +107,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_NUM] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, KC_HASH,    KC_7,    KC_8,    KC_9, KC_SLSH,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, KC_AMPR,    KC_4,    KC_5,    KC_6,  KC_EQL,                   XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_HYPR,
-  XXXXXXX, KC_ASTR,    KC_1,    KC_2,    KC_3, KC_BSLS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_TILD,    KC_4,    KC_5,    KC_6,  KC_EQL,                   XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_HYPR,
+  XXXXXXX, KC_SLSH,    KC_1,    KC_2,    KC_3, KC_BSLS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                              XXXXXXX,  KC_GRV,    KC_0, KC_MINS, XXXXXXX, _______, XXXXXXX, XXXXXXX
-),
-/* SYMBOL
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   ^  |   <  |  {   |  }   |  >   |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   $  |   !  |  (   |  )   |  +   |-------.    ,-------|      | SHIFT| CTRL | ALT  | SUPER|HYPER |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |   %  |   @  |  [   |  ]   |  |   |-------|    |-------|      |      |      |      |      |      |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      |   *  |   =  | /   _   /       \ SYM  \  |      |      |      |
- *                   |      |      |      |/       /         \      \ |      |      |      |
- *                   `----------------------------'           '------''--------------------'
- */
-
-
-[_SYMBOL] = LAYOUT(
-  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, KC_CIRC,   KC_LT,  KC_LCBR, KC_RCBR,   KC_GT,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX,  KC_DLR, KC_EXLM,  KC_LPRN, KC_RPRN, KC_PLUS,                    XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_HYPR,
-  XXXXXXX, KC_PERC,   KC_AT,  KC_LBRC, KC_RBRC, KC_PIPE, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                              XXXXXXX, KC_ASTR,  KC_EQL, KC_UNDS, _______,  XXXXXXX, XXXXXXX, XXXXXXX
 ),
 /* NAV
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -211,7 +211,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |SHIFT |   Z  |   X  |   C  |   V  |  B   |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | SHIFT|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      | ALT  |  SPC | /  TAB  /       \ ENTER\  | BKSPC| SUPER|      |
+ *                   |  ALT | ALT  |  SPC | /  TAB  /       \ ENTER\  | BKSPC| SUPER|      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -222,7 +222,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV, KC_Q, KC_W, KC_E, KC_R, KC_T,                    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,KC_MINUS,
   KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,                   KC_H,    KC_J,     KC_K,    KC_L, KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, XXXXXXX, XXXXXXX, KC_N,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH,  KC_GRV,
-                _______, KC_LALT,  KC_SPC,KC_TAB,  KC_ENT,   KC_BSPC, KC_LGUI,  _______
+                KC_LALT, KC_LALT,  KC_SPC,KC_TAB,  KC_ENT,   KC_BSPC, KC_LGUI,  _______
   ),
 };
 
@@ -237,25 +237,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 void matrix_scan_user(void) {
   achordion_task();
 }
-
-// Combos - Stolen from urobs ZMK Config
-enum combos {
-  // Left Hand Horizontal
-  WE_ESC,
-  ER_ENT,  
-  SD_TAB,
-  DF_REP,
-};
-
-// Left Hand Horizontal
-const uint16_t PROGMEM we_combo[] = {KC_W, KC_E, COMBO_END};
-const uint16_t PROGMEM er_combo[] = {KC_E, KC_R, COMBO_END};
-const uint16_t PROGMEM sd_combo[] = {KC_S, KC_D, COMBO_END};
-const uint16_t PROGMEM df_combo[] = {KC_D, KC_F, COMBO_END};
-
-combo_t key_combos[] = {
-  [WE_ESC] = COMBO(we_combo, KC_ESC),
-  [ER_ENT] = COMBO(er_combo, KC_ENT),
-  [SD_TAB] = COMBO(sd_combo, KC_TAB),
-  [DF_REP] = COMBO(df_combo, QK_REP),
-}; 
--- keyboards/boardsource/lulu/keymaps/diffusedlight/rules.mk
+++ keyboards/boardsource/lulu/keymaps/diffusedlight/rules.mk
@@ -2,4 +2,3 @@ ENCODER_MAP_ENABLE = no
 CAPS_WORD_ENABLE = yes
 REPEAT_KEY_ENABLE = yes
 SRC += features/achordion.c
-COMBO_ENABLE = yes
